15a6953cdb739a61caf2baf1cf4cc73175a367e9
ASWIN RAJ <greenmovies325@gmail.com>
2026-05-06 15:59:53 +0530

    Change greeting from 'Hello World' to 'Goodbye World'

--- Script.py
+++ Script.py
@@ -242,15 +242,8 @@ Nᴀᴍᴇ - {}"""
 
     CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
 
-<b>⚙️ sɪᴢᴇ : {file_size}</b>
-<b>📊 Quality: {quality}</b>
-<b>🎬 Season: {season}</b>| <b>Episode: {episode}</b>
-<b>🌐 Language: {language}</b>
-<b>📅 Year: {year}</b>
-<b>⏱️ Duration: {duration}</b>
-<b>📦 Format: {format}</b>
-  FILE BY :-@GREENMOVIEBOT
-  more more movies in any language follow as""" 
+<b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
+  FILE BY :-@GREENMOVIEBOT""" 
 
     IMDB_TEMPLATE_TXT = """
 <b>Query: {qurey}
